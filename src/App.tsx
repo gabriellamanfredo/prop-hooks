a6ec3f3e147133fb8a3b7c3354a6a290bd6d1aea
Gabriella Manfredo <127120301+gabriellamanfredo@users.noreply.github.com>
2024-04-26 14:24:41 -0300

    feat: prop-hooks

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Card from "./components/Card/Card"
+import Contador from "./components/Contador/Contador"
+import Task from "./components/Task/Task"
+
+function App() {
+  return (
+    <>
+    {/* <Card titulo="React" descricao="biblioteca com JS"/>
+
+    <Card titulo="Spring" descricao="Framework de Java"/> */}
+  
+    {/* <Contador/> */}
+
+      <Task/>
+    </>
+  )
+}
+
+export default App
